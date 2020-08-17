484172591a5deb88d1e96050672bc451c1950d16
Vijay Kumar <vijay.kumar.em51@gmail.com>
2020-08-17 13:00:24 +0530

    new commit

---
+++ src/main/java/com/cultclone/app/workoutscheduler/CultFitCloneWorkoutSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cultclone.app.workoutscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultFitCloneWorkoutSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultFitCloneWorkoutSchedulerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultclone/app/workoutscheduler/CultFitCloneWorkoutSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultclone.app.workoutscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultFitCloneWorkoutSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
